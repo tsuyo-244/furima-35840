cae4d6d7b1638663f0656b508bc9b621ad5cbca0
tsuyo-244 <84175792+tsuyo-244@users.noreply.github.com>
2021-06-17 17:43:36 +0900

    ユーザー管理機能修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
    has_many :items
-   has_many :buys
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/.freeze
   NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
     
