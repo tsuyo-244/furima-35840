65d9fe7bfa3def76f4eee96e5f5e11ce096df08d
tsuyo-244 <84175792+tsuyo-244@users.noreply.github.com>
2021-06-23 17:22:57 +0900

    修正５

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :comments
   devise_for :users
   root "items#index"
-  # resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create]
   # resources :buys, only:[:index, :new, :create] 
 end
