b4799cded58c85350f3df0e0bb5e21a5d6f9a4fb
tsuyo-244 <84175792+tsuyo-244@users.noreply.github.com>
2021-06-24 17:25:05 +0900

    修正2

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -5,8 +5,8 @@ function price() {
   const profit = document.getElementById("profit")
   itemprice.addEventListener('input', function(){
     const values = itemprice.value
-    addtaxprice.innerHTML = values * 0.1
+    addtaxprice.innerHTML =Math.floor( values * 0.1 )
     profit.innerHTML = values - addtaxprice.innerHTML
 })
 }
-window.addEventListener('turbolinks:load', price)
+window.addEventListener('load', price)
