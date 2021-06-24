62c3108bb8201b6e7bf2716c833b1412383c5919
tsuyo-244 <84175792+tsuyo-244@users.noreply.github.com>
2021-06-24 14:29:28 +0900

    Revert "Merge pull request #4 from tsuyo-244/revert-3-商品出品機能"
    This reverts commit 808688ed8e3faeead310ce8882f1750a23235206, reversing
    changes made to fe18ffaeb4c6172cbb9d4326192d59b5702a85f3.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!, except: [:index, :show, :search]
-  # before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
   def index
     # @items = Item.all.order('created_at DESC')
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :product, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_origin_id, :shipping_day_id, :price,).merge(user_id: current_user.id)
+  end
 end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,110 @@
+require 'rails_helper'
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品出品機能' do
+    context '商品出品ができる時' do
+      it '必須項目が全て存在すれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品ができない時' do
+      it '出品画像が空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空だと出品できない' do
+        @item.product = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product can't be blank")
+      end
+      it '商品説明が空だと出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーの情報が空だと出品できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'カテゴリーの情報で１を選択したら出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
+      end
+      it '商品の状態の情報が空だと出品できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '商品の状態の情報で１を選択したら出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition must be other than 1')
+      end
+      it '配送料の負担の情報が空だと出品できない' do
+        @item.shipping_cost_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
+      end
+      it '配送料の負担の情報で１を選択したら出品できない' do
+        @item.shipping_cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping cost must be other than 1')
+      end
+      it '発送元の地域の情報が空だと出品できない' do
+        @item.shipping_origin_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping origin can't be blank")
+      end
+      it '発送元の地域の情報で１を選択したら出品できない' do
+        @item.shipping_origin_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping origin must be other than 1')
+      end
+      it '発送日までの日数の情報が空だと出品できない' do
+        @item.shipping_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it '発送日までの日数の情報で１を選択したら出品できない' do
+        @item.shipping_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
+      end
+      it '価格が空だと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が¥300以上でないと出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it '価格が¥9,999,999以下でないと出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it '価格が半角数字でないと出品できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '商品価格が半角英数字混合では出品できない' do
+        @item.price = 'aa300'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '商品価格が半角英字のみでは出品できない' do
+        @item.price = 'aaaaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+    end
+  end
+end
