a03b3531493c8746e8ed155665b9718ce1b4b8fa
tsuyo-244 <84175792+tsuyo-244@users.noreply.github.com>
2021-06-25 17:19:01 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:edit,:show,:update]
+  before_action :move_to_index,only: [:edit,:update]
+  
   def index
     @items = Item.order("created_at DESC")
   end
@@ -18,11 +21,34 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :product, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_origin_id, :shipping_day_id, :price,).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @item.user_id 
+      redirect_to action: :index
+    end
   end
 end
